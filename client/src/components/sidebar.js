fcea0a97483beadd2989de7908a3c79f2b90f93e
leojrsilao <leojrsilao@gmail.com>
2019-11-27 12:35:34 -0500

    added sdebar

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import { Card} from 'react-bootstrap';
 
+
+// const coursesData = {
+//     name: 'My courses',
+//     toggled: true,
+//     children: [
+//         {
+//             name: 'ENGL10'
+//         },
+//         {
+//             name: 'FRAN10'
+//         },
+//         {
+//             name: 'MATH10'
+//         },
+//         {
+//             name: 'SCIE10'
+//         },
+//         {
+//             name: 'PHED10'
+//         },
+//         {
+//             name: 'ARTS10'
+//         }
+        
+//     ]
+// };
+
 export default class sidebar extends React.Component{
-    
+
     
 
+
     render(){
+        
         return(
             <div className="sidebar-component">
                 <Card> 
-                  <p>
-                  hi
-                  hi
-                  hi
-                  hihi
-                  hi :P
-                  hi
-                  hi
-                  </p>
+                    
                 </Card>
            </div>
         );
