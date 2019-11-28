07d98c5cbe1b058aaa0da809b216c202d70014f9
leojrsilao <leojrsilao@gmail.com>
2019-11-28 12:45:20 -0500

    enable cors for endpoint of courses

--- src/main/java/com/kimsquitsystem/kimsquitdemo/api/CourseController.java
+++ src/main/java/com/kimsquitsystem/kimsquitdemo/api/CourseController.java
@@ -3,12 +3,14 @@ package com.kimsquitsystem.kimsquitdemo.api;
 import com.kimsquitsystem.kimsquitdemo.model.Course;
 import com.kimsquitsystem.kimsquitdemo.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/course")
 @RestController
 public class CourseController {
