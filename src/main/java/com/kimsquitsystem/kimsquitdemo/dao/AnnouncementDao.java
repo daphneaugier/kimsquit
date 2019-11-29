9f5e601825305036d37df2bc8949c2e98a951202
ys-lin <yunshi.lin@shopify.com>
2019-11-29 10:17:52 -0500

    Fixed sql query error

--- src/main/java/com/kimsquitsystem/kimsquitdemo/dao/AnnouncementDao.java
+++ src/main/java/com/kimsquitsystem/kimsquitdemo/dao/AnnouncementDao.java
@@ -18,24 +18,24 @@ public class AnnouncementDao {
     }
 
     public List<Announcement> selectAllAnnouncements() {
-        final String sql = "SELECT announcement_id, course_id, date, announcement_text FROM announcement";
+        final String sql = "SELECT announcement_id, course_id, date, announcement FROM announcement";
         List<Announcement> announcements = jdbcTemplate.query(sql, (resultSet, i) -> {
             String announcement_id = resultSet.getString("announcement_id");
             String course_id = resultSet.getString("course_id");
             String date = resultSet.getString("date");
-            String announcement_text = resultSet.getString("announcement_text");
+            String announcement_text = resultSet.getString("announcement");
             return new Announcement(announcement_id, course_id, date, announcement_text);
         });
         return announcements;
     }
 
     public Optional<Announcement> selectAnnouncementByCourseId(String CourseId) {
-        final String sql = "SELECT announcement_id, announcement_name, date, announcement_description FROM announcement WHERE course_id = ?";
+        final String sql = "SELECT course_id, announcement_id,  date, announcement FROM announcement WHERE course_id = ?";
         Announcement announcement = jdbcTemplate.queryForObject(sql, new Object[]{ CourseId }, (resultSet, i) -> {
             String announcement_id = resultSet.getString("announcement_id");
             String course_id = resultSet.getString("course_id");
             String date = resultSet.getString("date");
-            String announcement_text = resultSet.getString("announcement_text");
+            String announcement_text = resultSet.getString("announcement");
             return new Announcement(announcement_id, course_id, date, announcement_text);
         });
         return Optional.ofNullable(announcement);
