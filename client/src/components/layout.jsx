55d31d324bd872c6013107431099d29343b76d7a
leojrsilao <leojrsilao@gmail.com>
2019-11-29 18:02:15 -0500

    display user from api on navbar

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -4,7 +4,7 @@ import Sidebar from './sidebar.jsx';
 import ContactInfo from './contactinfo.jsx';
 import Announcements from './announcements.jsx';
 import WeeklyInfo from './weeklyinfo.jsx';
-import {Card, Row, Col} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
 import './layout.css';
 
 
@@ -50,7 +50,7 @@ export default class layout extends React.Component{
                         </Row>
 
                         
-                        
+
                     </div>
                     
              
