1d85bf386b83bef656378eea67111e1833dcdf52
Jonathan Hsu <36024638+bit172@users.noreply.github.com>
2019-11-30 12:50:04 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,14 +1,21 @@
 -- Query for announcements for a specific course, order by latest
-SELECT announcement FROM announcement WHERE announcement.course_id = course.course_id ORDER BY DATETIME DESC;
+SELECT * FROM announcement a WHERE a.course_id = ? ORDER BY a.date DESC;
 
 -- Query the group name for a specific student
-SELECT name FROM groups WHERE groups.groups_id = student.group_id;
+SELECT g.name FROM kimsquit.groups g WHERE g.group_id = ?;
 
 -- Query for the grades in a specific course
-SELECT grade FROM grade WHERE grade.course_id = course.course_id;
+SELECT grade FROM grade WHERE grade.course_id = ?;
 
 -- Query all course_id's for a specific student
-SELECT course_id FROM course WHERE course.student_id = student.student_id;
+SELECT course_id 
+FROM course c
+NATURAL JOIN course_teacher
+NATURAL JOIN student 
+WHERE student_id = ?;
 
 -- Query teacher name, address for a specific course
-SELECT first_name, last_name, address FROM teacher WHERE teacher.teacher_id = course_teacher.teacher_id;
+SELECT first_name, last_name, address
+FROM teacher
+NATURAL JOIN course_teacher
+WHERE course_id = ?;
