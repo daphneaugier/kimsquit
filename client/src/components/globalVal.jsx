c1e93e82c70e95a5f0cba5fc8d866b532fe6fe8f
leojrsilao <leojrsilao@gmail.com>
2019-11-30 22:28:39 -0500

    announcements data used from db only

--- client/src/components/globalVal.jsx
+++ client/src/components/globalVal.jsx
@@ -1,3 +1,4 @@
+
 export default {
     studentId: Math.floor(Math.random() * 50) + 1,
     courseId: null
--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -27,7 +27,7 @@ export default function SidebarMenu() {
   useEffect(() => {
     const fetchData = async () => {
       const studentCourses = await axios(
-        `http://localhost:8080/api/v1/student/${globalVal.studentId}/course`,
+        `http://localhost:8080/api/v1/student/${globalVal.studentId}/course`
       );
       
       const Attendees = await axios(
